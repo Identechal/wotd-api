4a79a21ebf307c2163f8933fa3e49d4414ed4449
Nicholas Brokaw <brokawnd@gmail.com>
2020-01-18 12:30:51 -0800

    Created addWord, getWord, deleteWord
    Co-authored-by: Jacob Brokaw <brokawjn@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -12,23 +12,47 @@ const wordsRef = db.collection('words');
 exports.addWord = functions.https.onRequest(async (req, res) => {
 	const word = req.query.word;
 
+	if (word == null) {
+		res.send(400, `Please provide a word`);
+		return;
+	}
+
 	var wordExists = !(await wordsRef.where('word', '==', word).get()).empty;
 
 	if (!wordExists) {
 		const snapshot = await wordsRef.doc().set({ 'word': word });
-		res.send(201, 'Added ' + word);
+		res.send(201, `Added ${word}`);
 	}
 	else {
-		res.send(409, word + ' already exists');
+		res.send(409, `${word} already exists`);
 	}
 });
 
 exports.getWord = functions.https.onRequest(async (req, res) => {
 	const docs = await wordsRef.listDocuments();
 
-	var doc = docs[Math.round((Math.random() * docs.length))];
+	var doc = docs[Math.round((Math.random() * (docs.length - 1)))];
 
 	var word = (await doc.get()).get("word");
 
 	res.send(200, word);
+});
+
+exports.deleteWord = functions.https.onRequest(async (req, res) => {
+	const word = req.query.word;
+
+	var snapshot = await wordsRef.where('word', '==', word).get();
+
+	console.log(snapshot.empty);
+
+	if (snapshot.empty) {
+		res.send(200, `${word} doesn't exist`);
+		return;
+	}
+
+	snapshot.docs.forEach(async doc => {
+		await doc.ref.delete();
+	});
+
+	res.send(200, `${word} was deleted`);
 });
