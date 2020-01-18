8d77cc857946e83231ef0ceaaae4a65042646316
Nicholas Brokaw <brokawnd@gmail.com>
2020-01-17 18:16:49 -0800

    Created addWord endpoint and getWord endpoint.
    Co-authored-by: Jacob Brokaw <brokawjn@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,30 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+// 3rd Party
+const HttpStatus = require('http-status-codes');
 
 admin.initializeApp();
 
 const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const wordsRef = db.collection('words');
 
 
-exports.addMessage = funcitons.https.onRequest(async (req, res) =>  {
-    const original = req.query.text;
 
+exports.addWord = functions.https.onRequest(async (req, res) => {
+	const original = req.query.word;
 
-})
+	const snapshot = await wordsRef.doc().set({ "word": original });
+
+	res.sendStatus(200);
+});
+
+exports.getWord = functions.https.onRequest(async (req, res) => {
+	const docs = await wordsRef.listDocuments();
+
+	var doc = docs[Math.round((Math.random() * docs.length))];
+
+	var word = (await doc.get()).get("word");
+
+	res.send(200, word);
+});
