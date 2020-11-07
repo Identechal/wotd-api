fad96c06bad31f70b2d287d46dca3dc6324c5b55
Jacob Brokaw <brokawjn@gmail.com>
2020-11-07 14:29:01 -0600

    Fixed node-forge vulnerability

--- functions/index.js
+++ functions/index.js
@@ -2,70 +2,55 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-
 const db = admin.firestore();
-
 const wordsRef = db.collection('words');
-
 const letters = /^[A-Za-z]+$/;
 
 exports.addWord = functions.https.onRequest(async (req, res) => {
 	var word = req.query.word;
-
 	if (word == null) {
 		res.send(400, `Please provide a word`);
 		return;
 	}
-
 	word = word.trim().toLowerCase();
 
 	if (word.length == 0) {
 		res.send(400, `Words must have at least one letter`);
 		return;
-	}
-	else if (!word.match(letters)) {
+	} else if (!word.match(letters)) {
 		res.send(400, `Words must only contain letters`);
 		return;
 	}
 
 	var wordExists = !(await wordsRef.where('word', '==', word).get()).empty;
-
 	if (!wordExists) {
-		const snapshot = await wordsRef.doc().set({ 'word': word });
+		const snapshot = await wordsRef.doc().set({ word: word });
 		res.send(201, `Added ${word}`);
-	}
-	else {
-		res.send(409, `${word} already exists`);
-	}
+	} else res.send(409, `${word} already exists`);
 });
 
 exports.deleteWord = functions.https.onRequest(async (req, res) => {
 	var word = req.query.word;
-
 	if (word == null) {
 		res.send(400, `Please provide a word`);
 		return;
 	}
-
 	word = word.trim().toLowerCase();
 
 	if (word.length == 0) {
 		res.send(400, `Words must have at least one letter`);
 		return;
-	}
-	else if (!word.match(letters)) {
+	} else if (!word.match(letters)) {
 		res.send(400, `Words must only contain letters`);
 		return;
 	}
 
 	var snapshot = await wordsRef.where('word', '==', word).get();
-
 	if (snapshot.empty) {
 		res.send(200, `${word} doesn't exist`);
 		return;
 	}
-
-	snapshot.docs.forEach(async doc => {
+	snapshot.docs.forEach(async (doc) => {
 		await doc.ref.delete();
 	});
 
@@ -74,11 +59,7 @@ exports.deleteWord = functions.https.onRequest(async (req, res) => {
 
 exports.getWord = functions.https.onRequest(async (req, res) => {
 	const docs = await wordsRef.listDocuments();
-
-	var doc = docs[Math.round((Math.random() * (docs.length - 1)))];
-
-	var word = (await doc.get()).get("word");
-
+	var doc = docs[Math.round(Math.random() * (docs.length - 1))];
+	var word = (await doc.get()).get('word');
 	res.send(200, word);
 });
-
