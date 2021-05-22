7643e0cf829caf2015700f5879941b33f1be201b
Jacob Brokaw <brokawjn@gmail.com>
2021-05-22 09:49:28 -0500

    Use status().send()

--- functions/index.js
+++ functions/index.js
@@ -8,69 +8,69 @@ const wordsRef = db.collection('words');
 const letters = /^[A-Za-z]+$/;
 
 exports.addWord = functions.https.onRequest(async (req, res) => {
-  if (req.header('Api-Key') != key) {
-    res.send(403, `Invalid API key`);
-    return;
-  }
+	if (req.header('Api-Key') != key) {
+		res.status(403).send(`Invalid API key`);
+		return;
+	}
 
-  var word = req.query.word;
-  if (word == null) {
-    res.send(400, `Please provide a word`);
-    return;
-  }
-  word = word.trim().toLowerCase();
+	var word = req.query.word;
+	if (word == null) {
+		res.status(400).send(`Please provide a word`);
+		return;
+	}
+	word = word.trim().toLowerCase();
 
-  if (word.length == 0) {
-    res.send(400, `Words must have at least one letter`);
-    return;
-  } else if (!word.match(letters)) {
-    res.send(400, `Words must only contain letters`);
-    return;
-  }
+	if (word.length == 0) {
+		res.status(400).send(`Words must have at least one letter`);
+		return;
+	} else if (!word.match(letters)) {
+		res.status(400).send(`Words must only contain letters`);
+		return;
+	}
 
-  var wordExists = !(await wordsRef.where('word', '==', word).get()).empty;
-  if (!wordExists) {
-    const snapshot = await wordsRef.doc().set({ word: word });
-    res.send(201, `Added ${word}`);
-  } else res.send(409, `${word} already exists`);
+	var wordExists = !(await wordsRef.where('word', '==', word).get()).empty;
+	if (!wordExists) {
+		const snapshot = await wordsRef.doc().set({ word: word });
+		res.status(201).send(`Added ${word}`);
+	} else res.status(409).send(`${word} already exists`);
 });
 
 exports.deleteWord = functions.https.onRequest(async (req, res) => {
-  if (req.header('Api-Key') != key) {
-    res.send(403, `Invalid API key`);
-    return;
-  }
+	if (req.header('Api-Key') != key) {
+		res.status(403).send(`Invalid API key`);
+		return;
+	}
 
-  var word = req.query.word;
-  if (word == null) {
-    res.send(400, `Please provide a word`);
-    return;
-  }
-  word = word.trim().toLowerCase();
+	var word = req.query.word;
+	if (word == null) {
+		res.status(400).send(`Please provide a word`);
+		return;
+	}
+	word = word.trim().toLowerCase();
 
-  if (word.length == 0) {
-    res.send(400, `Words must have at least one letter`);
-    return;
-  } else if (!word.match(letters)) {
-    res.send(400, `Words must only contain letters`);
-    return;
-  }
+	if (word.length == 0) {
+		res.status(400).send(`Words must have at least one letter`);
+		return;
+	} else if (!word.match(letters)) {
+		res.status(400).send(`Words must only contain letters`);
+		return;
+	}
 
-  var snapshot = await wordsRef.where('word', '==', word).get();
-  if (snapshot.empty) {
-    res.send(200, `${word} doesn't exist`);
-    return;
-  }
-  snapshot.docs.forEach(async (doc) => {
-    await doc.ref.delete();
-  });
+	var snapshot = await wordsRef.where('word', '==', word).get();
+	if (snapshot.empty) {
+		res.status(200).send(`${word} doesn't exist`);
+		return;
+	}
+	snapshot.docs.forEach(async (doc) => {
+		await doc.ref.delete();
+	});
 
-  res.send(200, `${word} was deleted`);
+	res.status(200).send(`${word} was deleted`);
 });
 
 exports.getWord = functions.https.onRequest(async (req, res) => {
-  const docs = await wordsRef.listDocuments();
-  var doc = docs[Math.round(Math.random() * (docs.length - 1))];
-  var word = (await doc.get()).get('word');
-  res.send(200, word);
+	const docs = await wordsRef.listDocuments();
+	var doc = docs[Math.round(Math.random() * (docs.length - 1))];
+	var word = (await doc.get()).get('word');
+	res.status(200).send(word);
 });
